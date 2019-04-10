4bc02219538ba25246d9bdd4a2981e41186922ce
Kafui Kpoh <qaphuikpoh@gmail.com>
2019-04-10 08:08:24 +0000

    Added Product Domain and controller

---
+++ DemoEcommerceApp/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoEcommerceApp.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoEcommerceApp.Controllers
+{
+    [Produces ("application/json")]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        // GET: api/Products
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return new Product[]
+            {
+                new Product (1, "Oats", new decimal(3.07)),
+                new Product (2, "ToothPaste", new decimal(10.89)),
+                new Product (3, "Television", new decimal(500.90))
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/Products
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Product product) 
+            => (await )
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+
+    }
+
+
+}
