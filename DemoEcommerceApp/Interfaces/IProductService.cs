7af0ad6e46db0a59def830a44827fa2a02b0cc29
Kafui Kpoh <qaphuikpoh@gmail.com>
2019-04-10 08:36:04 +0000

    Added the IProductService interface

---
+++ DemoEcommerceApp/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using DemoEcommerceApp.Domain;
+
+namespace DemoEcommerceApp.Interfaces
+{
+    public interface IProductService
+    {
+        Task<bool> CreateProductAsync(Product product);
+        Task<Product> GetOrderAsync(int id);
+        Task<bool> UpdateProductAsync(int id, Product product);
+        Task<bool> DeleteOrderAsync(int id);
+    }
+}
